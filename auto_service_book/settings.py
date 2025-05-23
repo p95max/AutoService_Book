bbefe1c4f426975025e711410d01f4c8dcc1f6fb
Maxx <m.petrykin@gmx.de>
2025-05-24 00:20:30 +0200

    first commit

---
+++ auto_service_book/settings.py
@@ -0,0 +1,143 @@
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# SECURITY SETTINGS
+SECRET_KEY = 'django-insecure-(k&6mh&i=(13+%ti460nndeif*%+!^defyp87$9#ksn87x7u$@'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+
+# APPLICATIONS
+INSTALLED_APPS = [
+    # Django core
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'allauth',
+    'allauth.account',
+    'crispy_forms',
+    'crispy_bootstrap5',
+
+    'service_book',
+
+]
+
+
+# MIDDLEWARE
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
+]
+
+
+# URLS & TEMPLATES
+ROOT_URLCONF = 'auto_service_book.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'service_book/templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'auto_service_book.wsgi.application'
+
+
+# DATABASE
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# AUTHENTICATION
+SITE_ID = 1
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_LOGIN_METHODS = {'email'}
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'password1*', 'password2*']
+
+LOGIN_REDIRECT_URL = '/'
+ACCOUNT_LOGOUT_REDIRECT_URL = '/'
+
+
+
+# PASSWORD VALIDATION
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# LOCALIZATION
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+
+# STATIC FILES
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
+# DEFAULT PRIMARY KEY
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# Email
+
+# --- Email Settings ---
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER='maxpetrikin@gmail.com'
+EMAIL_HOST_PASSWORD='vahn-irxa-rlon-qjoi'
+DEFAULT_FROM_EMAIL='maxpetrikin@gmail.com'
---
+++ service_book/models.py
@@ -0,0 +1,55 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} - {self.country}"
+
+
+class Car(models.Model):
+    brand = models.ManyToManyField(Brand)
+    model = models.CharField(max_length=100)
+    prod_year = models.IntegerField()
+    miliage = models.IntegerField()
+    vin = models.CharField(max_length=20, unique=True)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        default=None,
+        verbose_name='Owner'
+    )
+
+    def __str__(self):
+        return f"{self.model} - {self.owner}"
+
+
+class ServiceRecord(models.Model):
+    SERVICE_ACTIONS = [
+        ('interval_service', 'Interval Service'),
+        ('diagnostics', 'Diagnostic'),
+        ('tire_service', 'Tire Service'),
+        ('repair_service', 'Repair'),
+        ('other_service', 'Other'),
+    ]
+
+    date = models.DateField()
+    car = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True)
+    price = models.IntegerField()
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='User'
+    )
+    place = models.CharField(max_length=100)
+    service_type = models.CharField(max_length=50, choices=SERVICE_ACTIONS)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.car} - {self.get_service_type_display()}"
